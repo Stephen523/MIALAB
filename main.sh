703aa56dd24dc1a584b8a3885c0a0157aaa3be96
unknown <1986307064@qq.com>
2023-10-31 15:51:30 +0800

    Init commit

---
+++ main.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ -e Program/naccess2.1.1 ];then
+	chmod u+x Program/naccess2.1.1/*
+fi
+if [ -e Program/Qcontacts ];then
+	        chmod u+x Program/Qcontacts/*
+fi
+bash work/clear_file.sh
+bash work/divide_patch.sh
+bash work/sort_patch.sh
