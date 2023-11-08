a93ae416d5dc51fbed94b42e2f5f7b54060dc985
unknown <1986307064@qq.com>
2023-11-08 21:12:51 +0800

    Update

--- main.sh
+++ main.sh
@@ -5,6 +5,9 @@ fi
 if [ -e Program/Qcontacts ];then
 	        chmod u+x Program/Qcontacts/*
 fi
+cd Program/naccess2.1.1/
+csh install.scr
+cd ../../
 bash work/clear_file.sh
 bash work/divide_patch.sh
 bash work/sort_patch.sh
